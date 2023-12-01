3a1c42be5f599621689e84b82ebd63c593592c54
Ali Al Hubail <swe.amh77@gmail.com>
2023-12-01 15:35:01 +0300

    Add EventsTracking

---
+++ lib/common_widgets/custom_event_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CustomEventWidget extends StatelessWidget {
+  const CustomEventWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
