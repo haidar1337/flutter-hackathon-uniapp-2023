5a0fc4a29cb69a145654b2370bf262a48c1fc1e4
Ahmed Talal <hamad5232@yahoo.ca>
2023-12-01 12:35:54 +0300

    adding the student card

--- lib/features/profile/presentation/profile_page.dart
+++ lib/features/profile/presentation/profile_page.dart
@@ -97,23 +97,159 @@ class _ProfilePageState extends State<ProfilePage> {
                   ),
                 ),
                 const SizedBox(height: 10),
-                Container(
-                  padding: const EdgeInsets.all(8.0),
-                  height: 270,
-                  width: double.maxFinite,
-                  decoration: BoxDecoration(
-                      color: Colors.transparent,
-                      border: Border.all(
-                        color: rectBackColor,
-                      ),
-                      borderRadius:
-                          const BorderRadius.all(Radius.circular(30))),
-                ),
+                StudentCard(),
               ],
             ),
           )
         ],
       ),
+    );
+  }
+}
+
+const textStyle = TextStyle(fontSize: 15, fontWeight: FontWeight.bold);
+
+class StudentCard extends StatelessWidget {
+  const StudentCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20.0),
+      height: 324,
+      width: double.maxFinite,
+      decoration: BoxDecoration(
+        color: Colors.transparent,
+        border: Border.all(
+          color: rectBackColor,
+        ),
+        borderRadius: const BorderRadius.all(Radius.circular(30)),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 100,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.red.shade900,
+                  borderRadius:
+                      const BorderRadius.only(topLeft: Radius.circular(40)),
+                ),
+                child: const Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "طالب",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      "Studetn",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(width: 5),
+              Expanded(
+                child: Container(
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.green.shade200,
+                    borderRadius:
+                        const BorderRadius.only(topRight: Radius.circular(40)),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      "King Fahd University",
+                      style: textStyle,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text("202017740", style: textStyle),
+              const SizedBox(width: 10),
+              Container(
+                color: Colors.green.shade200,
+                width: 2,
+                height: 40,
+              ),
+              const SizedBox(width: 10),
+              const Column(
+                children: [
+                  Text(
+                    "أحمد طلال",
+                    style: textStyle,
+                  ),
+                  Text(
+                    "Ahmed Talal",
+                    style: textStyle,
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Divider(color: Colors.green.shade200, thickness: 1),
+          rowGenetator("Govt.ID", "السجل المدني", "1116607423", "١١١٦٦٠٧٤٢٣"),
+          Divider(color: Colors.green.shade200, thickness: 1),
+          rowGenetator("Nationality", "الجنسية", "Saudi", "سعودي"),
+          Divider(color: Colors.green.shade200, thickness: 1),
+          rowGenetator(
+            "Date of Birth",
+            "تاريخ الميلاد",
+            "25/6/2002",
+            "14/4/1423",
+          ),
+        ],
+      ),
+    );
+  }
+
+  Row rowGenetator(
+      String titleEng, String titleArb, String valueEng, String valueArb) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Text(valueEng, style: textStyle),
+        const SizedBox(width: 10),
+        Container(
+          color: Colors.green.shade200,
+          width: 2,
+          height: 40,
+        ),
+        const SizedBox(width: 10),
+        Text(valueArb, style: textStyle),
+        const SizedBox(width: 10),
+        Container(
+          color: Colors.green.shade200,
+          width: 2,
+          height: 40,
+        ),
+        const SizedBox(width: 10),
+        Column(
+          children: [
+            Text(titleArb, style: textStyle),
+            Text(titleEng, style: textStyle),
+          ],
+        ),
+      ],
     );
   }
 }
