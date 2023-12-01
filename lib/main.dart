7f2aa3d28cca44d7f9d4d22132fdff8eb2baa81a
Ali Al Hubail <swe.amh77@gmail.com>
2023-12-01 16:39:09 +0300

    Modify EventPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:projectname/features/tareef/presentation/evaluation.dart';
+import 'package:projectname/features/events/data/event_data.dart';
+import 'package:projectname/features/events/presentation/event.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Event App',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color.fromARGB(255, 0, 125, 64)),
-        useMaterial3: true,
+        backgroundColor: Colors.green,
       ),
-      home: Evaluation(),
+      home: Event(
+        eventData: EventData(
+          name: 'ابو حسوة',
+          location: 'شطيرتي',
+          date: DateTime.parse('2023-12-10'),
+        ),
+      ),
     );
   }
 }
