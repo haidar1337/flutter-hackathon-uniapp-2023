de5a36e46d9ac231a93123e07b4dbeea73b25427
Ali Al Hubail <swe.amh77@gmail.com>
2023-12-02 14:34:55 +0300

    Add the logic of the GPA

--- lib/common_widgets/custom_grade_card.dart
+++ lib/common_widgets/custom_grade_card.dart
@@ -17,7 +17,46 @@ class CustomGradeCard extends StatelessWidget {
               fontWeight: FontWeight.bold,
               fontSize: 20)),
       onTap: () {
-        // Handle tap
+        showModalBottomSheet(
+          context: context,
+          builder: (context) => SizedBox(
+            width: double.infinity,
+            height: 300, // Adjusted height for better spacing
+            child: Padding(
+              padding: const EdgeInsets.all(12.0), // Padding for overall modal
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Center(
+                    child: Text(course[index]["name"],
+                        style: const TextStyle(
+                            fontSize: 22, fontWeight: FontWeight.bold)),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(course[index]["description"],
+                      style: const TextStyle(fontSize: 18)),
+                  const SizedBox(height: 15),
+                  Text("Quizzes: ${course[index]['quizzes']}",
+                      style: const TextStyle(fontSize: 16)),
+                  const SizedBox(height: 8),
+                  Text("Mid-Term: ${course[index]['midterm']}",
+                      style: const TextStyle(fontSize: 16)),
+                  const SizedBox(height: 8),
+                  Text("Homeworks: ${course[index]['homeworks']}",
+                      style: const TextStyle(fontSize: 16)),
+                  const SizedBox(height: 8),
+                  Text("Final: ${course[index]['final']}",
+                      style: const TextStyle(fontSize: 16)),
+                  const SizedBox(height: 15),
+                  Text("Grade: ${course[index]["grade"]}",
+                      style: const TextStyle(
+                          fontSize: 20, fontWeight: FontWeight.bold)),
+                ],
+              ),
+            ),
+          ),
+        );
       },
       icon: Icons.book,
     );
@@ -26,25 +65,25 @@ class CustomGradeCard extends StatelessWidget {
   Color _getGradeColor(String grade) {
     switch (grade) {
       case "A+":
-        return Colors.blue; // Starting A+ with blue
+        return Colors.blue[800]!; // Deeper blue for A+
       case "A":
-        return Colors.lightBlueAccent; // A lighter shade of blue for A
+        return Colors.blue[700]!; // Deep blue for A
       case "B+":
-        return Colors.greenAccent; // A lighter shade of green for B+
+        return Colors.green[600]!; // Deep green for B+
       case "B":
-        return Colors.green; // Green for B
+        return Colors.green[500]!; // Darker green for B
       case "C+":
-        return Colors.yellowAccent; // Bright yellow for C+
+        return Colors.amber[700]!; // Deep amber for C+
       case "C":
-        return Colors.yellow; // Standard yellow for C
+        return Colors.amber[600]!; // Darker amber for C
       case "D+":
-        return Colors.orangeAccent; // Lighter orange for D+
+        return Colors.deepOrange[700]!; // Deep orange for D+
       case "D":
-        return Colors.orange; // Standard orange for D
+        return Colors.deepOrange[600]!; // Darker orange for D
       case "F":
-        return Colors.red; // Red for F
+        return Colors.red[800]!; // Deep red for F
       default:
-        return Colors.grey; // Grey for undefined grades
+        return Colors.grey[600]!; // Darker grey for undefined grades
     }
   }
 }
--- lib/features/grades/presentation/grade_page.dart
+++ lib/features/grades/presentation/grade_page.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:projectname/common_widgets/custom_card_widget.dart';
 import 'package:projectname/common_widgets/custom_grade_card.dart';
@@ -18,6 +19,51 @@ class _GradesPageState extends State<GradesPage> {
     'Term 232'
   ];
   String? selectedTerm;
+  double gpa = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    // Assuming the initial GPA calculation is needed
+    gpa = calculateGPA(CourseData.sampleData);
+  }
+
+  double calculateGPA(List<dynamic> courses) {
+    if (courses.isEmpty) {
+      return 0.0;
+    }
+
+    double totalPoints = 0;
+    for (var course in courses) {
+      totalPoints += _gradeToPoint(course["grade"]);
+    }
+    return totalPoints / courses.length;
+  }
+
+  double _gradeToPoint(String grade) {
+    switch (grade) {
+      case "A+":
+        return 4.0;
+      case "A":
+        return 3.75;
+      case "B+":
+        return 3.5;
+      case "B":
+        return 3.0;
+      case "C+":
+        return 2.5;
+      case "C":
+        return 2.0;
+      case "D+":
+        return 1.5;
+      case "D":
+        return 1.0;
+      case "F":
+        return 0.0;
+      default:
+        return 0.0; // Default for undefined grades
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -53,6 +99,9 @@ class _GradesPageState extends State<GradesPage> {
                   onChanged: (String? newValue) {
                     setState(() {
                       selectedTerm = newValue;
+                      // Update the GPA based on the selected term
+                      // This is just an example. You might need to fetch the courses for the selected term.
+                      gpa = calculateGPA(CourseData.sampleData);
                     });
                   },
                   items: terms.map<DropdownMenuItem<String>>((String value) {
@@ -68,20 +117,17 @@ class _GradesPageState extends State<GradesPage> {
           Expanded(
             child: ListView(
               children: [
-                // GPA & Badge Card
                 CustomCardWidget(
                   title: "GPA & Badge",
-                  subtitle:
-                      const Text("GPA: 3.75", style: TextStyle(fontSize: 20)),
+                  subtitle: Text("GPA: ${gpa.toStringAsFixed(2)}",
+                      style: const TextStyle(fontSize: 20)),
                   icon: Icons.school,
                   onTap: () {
                     // Handle tap
                   },
                 ),
-                // Assuming 'courses' is a list of course data
-                // Replace this with your actual data source
                 ...List.generate(
-                    3,
+                    CourseData.sampleData.length,
                     (index) => CustomGradeCard(
                           course: CourseData.sampleData,
                           index: index,
@@ -97,17 +143,37 @@ class _GradesPageState extends State<GradesPage> {
 
 // Mock class for CourseData, replace with your actual data model
 class CourseData {
+  static final _random = Random();
+
+  static int _randomScore(int max) => _random.nextInt(max) + 1;
+
   static get sampleData => [
-        {"name": "ICS 321", "description": "Database Systems", "grade": "A+"},
+        {
+          "name": "ICS 321",
+          "description": "Database Systems",
+          "grade": "A+",
+          "quizzes": _randomScore(10), // Random score out of 10
+          "midterm": _randomScore(25), // Random score out of 25
+          "homeworks": _randomScore(15), // Random score out of 15
+          "final": _randomScore(30), // Random score out of 30
+        },
         {
           "name": "GS 332 ",
           "description": "Principles of Sociology",
-          "grade": "A"
+          "grade": "A+",
+          "quizzes": _randomScore(10),
+          "midterm": _randomScore(25),
+          "homeworks": _randomScore(15),
+          "final": _randomScore(30),
         },
         {
           "name": "SWE 316",
           "description": "Software Design and Construction",
-          "grade": "A+"
+          "grade": "A+",
+          "quizzes": _randomScore(10),
+          "midterm": _randomScore(25),
+          "homeworks": _randomScore(15),
+          "final": _randomScore(30),
         }
-      ]; // Replace with actual data structure
+      ];
 }
